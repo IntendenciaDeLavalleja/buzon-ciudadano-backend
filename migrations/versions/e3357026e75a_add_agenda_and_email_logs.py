a55c6f5ef91d9a68d1fe7e916a33ca20f603d7f6
IntendenciaDeLavalleja <repositorioIDL@lavalleja.gub.uy>
2026-03-19 15:32:24 -0300

    se corrigieron errores de compilación de typescript

--- migrations/versions/e3357026e75a_add_agenda_and_email_logs.py
+++ migrations/versions/e3357026e75a_add_agenda_and_email_logs.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'e3357026e75a'
-down_revision = None
+down_revision = '0000_initial_schema'
 branch_labels = None
 depends_on = None
 
